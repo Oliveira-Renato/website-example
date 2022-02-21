69b12b037900e7f79e6212b53170465950632948
oliveira-renato <renato.printf@gmail.com>
2022-02-20 22:07:47 -0300

    working on footer yet, but it looks better now

--- src/pages/footer/index.js
+++ src/pages/footer/index.js
@@ -4,6 +4,7 @@ import { FaCopyright } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 
 
@@ -15,32 +16,61 @@ import syscareImg from '../../images/iconsLogo/logo_syscare_200.png'
 
 export function Footer(){
   return(
-    <footer className="footer">
-      <div className="footer__social">
-        <a href="">
-          <FaInstagram size="40"  color="#FFF"/>
-        </a>
-        <a href="">
-          <FaYoutube size="40"  color="#FFF"/>
-        </a>
-        <a href="">
-          <FaLinkedin size="40"  color="#FFF"/>
-        </a>    
-      </div>
-      
+      // <div className="footer__social">
+      //   <a href="">
+      //     <FaInstagram size="40"  color="#FFF"/>
+      //   </a>
+      //   <a href="">
+      //     <FaYoutube size="40"  color="#FFF"/>
+      //   </a>
+      //   <a href="">
+      //     <FaLinkedin size="40"  color="#FFF"/>
+      //   </a>    
+      // </div>
 
-      <div className="footer__allTogether">
-        <div className="kennenFooter">
-          <img src={ kennenImg } />
+
+    
+  <footer className="footer">
+    <div className="container">
+      <div className="row">
+        <div className="footer-col">
+          <h4>Empresa</h4>
+          <ul>
+            <li><a href="#">Sobre nos</a></li>
+            <li><a href="#">Nossos serviços</a></li>
+            <li><a href="#">Política de privacidade</a></li>
+            <li><a href="#">affiliate program</a></li>
+          </ul>
         </div>
-        <div className="syscareFooter">
-          <img src={ syscareImg } />
+        <div className="footer-col">
+          <h4>Suporte</h4>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Suporte</a></li>
+            <li><a href="#">returns</a></li>
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h4>Produtos</h4>
+          <ul>
+            <li><a href="#">watch</a></li>
+            <li><a href="#">bag</a></li>
+            <li><a href="#">shoes</a></li>
+            <li><a href="#">dress</a></li>
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h4>Siga nos</h4>
+          <div className="social-links">
+            <a href="#"><i className="fab fa-facebook-f"></i></a>
+            <a href="#"><i className="fab fa-twitter"></i></a>
+            <a href="#"><i className="fab fa-instagram"></i></a>
+            <a href="#"><i className="fab fa-linkedin-in"></i></a>
+          </div>
         </div>
       </div>
-
-      <p className="footer__copyright">2022 by <FaCopyright color="#FFF"/> Kennen Consultoria e Sistemas de Informação Ltda.</p>
-    </footer>
-
+    </div>
+  </footer>
     
   )
 }
