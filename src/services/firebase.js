8ec7d30e786e13ea677ab3d02fd0092591ce3a1e
Oliveira-Renato <renato.printf@gmail.com>
2023-01-09 15:36:16 -0300

    firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID ,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_APP_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
